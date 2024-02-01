f822afbc5d7d9e930937742ce77b718fd101d0a0
yamatoure <nagata.engineer@gmail.com>
2024-02-02 01:28:26 +0900

    タイトルカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_01_143850) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_01_162622) do
   create_table "groups", charset: "utf8", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title", null: false
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
