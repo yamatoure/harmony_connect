00b3a9eac1d500892b698f0073c68c8bd72b0177
yamatoure <nagata.engineer@gmail.com>
2024-02-07 00:42:25 +0900

    メンバー募集投稿機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    
+    @groups = Group.all
   end
 
   def new
@@ -9,6 +9,16 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  private
+  def group_params
+    params.require(:group).permit(:title, :content, area_ids: [], part_ids: []).merge(user_id: current_user.id)
   end
 end
