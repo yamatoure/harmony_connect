fa2e7e7062f862036c51e5ce911789a08a4f37a5
yamatoure <nagata.engineer@gmail.com>
2024-02-06 23:58:17 +0900

    バリデーションの設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ApplicationRecord
   has_many :areas, through: :group_areas
   has_many :group_parts
   has_many :parts, through: :group_parts
+
+  validates :title,    presence: true
+  validates :area_ids, presence: true
+  validates :part_ids, presence: true
 end
