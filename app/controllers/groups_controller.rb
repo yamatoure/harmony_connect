f849abd20b529fc699f90ddf0bb6df8c4ceb5c34
yamatoure <nagata.engineer@gmail.com>
2024-02-08 00:05:44 +0900

    メンバー募集編集機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,6 +23,22 @@ class GroupsController < ApplicationController
     @parts = @group.parts
   end
 
+  def edit
+    @group = Group.find(params[:id])
+    unless current_user.id == @group.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
   private
   def group_params
     params.require(:group).permit(:title, :content, area_ids: [], part_ids: []).merge(user_id: current_user.id)
