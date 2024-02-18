a8a37632342d59cfbd5a60efbac6722d3c66196a
yamatoure <nagata.engineer@gmail.com>
2024-02-18 12:37:18 +0900

    ユーザーモデルのアソシエーション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :groups
+  has_many :groups, dependent: :destroy
 
   validates :name, presence: true
 end
